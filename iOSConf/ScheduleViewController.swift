b029db6bc5d5863afffe2ba4c31212162417bd03
Nikos Maounis <nikospsy2k@gmail.com>
2017-02-04 22:50:38 +0200

    Cleanup

--- iOSConf/ScheduleViewController.swift
+++ iOSConf/ScheduleViewController.swift
@@ -89,15 +89,5 @@ class ScheduleViewController: ConferenceViewController, UITableViewDelegate, UIT
             return cell
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

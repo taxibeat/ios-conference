adb8e97c208a149d7823a6960fcfca99a54962b5
Nikos Maounis <nikospsy2k@gmail.com>
2017-02-04 16:19:17 +0200

    Added CloudKit manager

---
+++ iOSConf/CloudKitManager.swift
@@ -0,0 +1,51 @@
+//
+//  CloudKitManager.swift
+//  iOSConf
+//
+//  Created by Nikos Maounis on 04/02/2017.
+//  Copyright © 2017 Taxibeat Ltd. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class CloudKitManager: NSObject {
+    static let sharedInstance = CloudKitManager()
+    
+    func fetchSpeakers(_ completion:@escaping (_ speakers:[Speaker]?, _ error: Error?) -> ()) {
+        var items: [CKRecord] = [CKRecord]()
+        let query = CKQuery(recordType: "Speakers", predicate: NSPredicate(format: "TRUEPREDICATE"))
+        let queryOperation = CKQueryOperation(query: query)
+        
+        Log.printThis("Start fetch")
+        
+        queryOperation.recordFetchedBlock = { (record: CKRecord!) -> () in
+            items.append(record)
+        }
+        
+        queryOperation.qualityOfService = .userInteractive
+        
+        queryOperation.queryCompletionBlock = { (cursor: CKQueryCursor?, error: Error?) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                }
+                completion(nil, error)
+            } else {
+                var speakerArray = [Speaker]()
+                
+                for item in items {
+                    let speaker = Speaker(record: item)
+                    
+                    if let newSpeaker = speaker {
+                        speakerArray.append(newSpeaker)
+                    }
+                }
+                completion(speakerArray, nil)
+            }
+        }
+        
+        let database: CKDatabase = CKContainer.default().publicCloudDatabase
+        database.add(queryOperation)
+    }
+}
